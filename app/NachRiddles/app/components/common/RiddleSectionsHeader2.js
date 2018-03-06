07024778d67d4e97999c86ef791ee9b0b2ea731a
GuyZwer <guy.zwerdling@gmail.com>
2018-03-06 22:55:01 +0200

    update the parallel section to upload modal

--- app/NachRiddles/app/components/common/RiddleSectionsHeader2.js
+++ app/NachRiddles/app/components/common/RiddleSectionsHeader2.js
@@ -1,36 +1,57 @@
-import React, {Component} from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
+import Modal from 'react-native-modalbox'
 
-
-class RiddleSectionsHeader2 extends Component{
-  render(){
-    return(
+class RiddleSectionsHeader2 extends Component {
+  parallel() {
+    this.refs.modalParallel.open()
+  }
+  render() {
+    return (
       <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>{this.props.headerText}</Text>
+        <Modal
+          style={[styles.modal, styles.modalParallel]}
+          position={'center'}
+          ref={'modalParallel'}
+        />
+        <TouchableOpacity onPress={() => this.parallel()}>
+          <Text style={styles.textStyle}>{this.props.headerText}</Text>
+        </TouchableOpacity>
       </View>
     )
   }
-};
+}
 
 const styles = {
-  viewStyle:{
-    flex:2,
-    alignItems:'center',
+  viewStyle: {
+    flex: 2,
+    alignItems: 'center',
     backgroundColor: '#472747',
     borderColor: 'black',
     borderWidth: 2,
     height: 60,
-    margin:0,
+    margin: 0,
     flexWrap: 'wrap',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   textStyle: {
     fontFamily: 'nrkis',
     textAlign: 'center',
-    fontSize:15,
-    color: 'white',
+    fontSize: 15,
+    color: 'white'
+  },
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalParallel: {
+    backgroundColor: '#2196F3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 3,
+    borderRadius: 10,
+    height: 200
   }
+}
 
-};
-
-export {RiddleSectionsHeader2};
+export { RiddleSectionsHeader2 }
