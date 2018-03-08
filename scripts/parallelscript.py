af8f056907e02ee1ab9c283838127298efa6b78a
guy.zwerdling <guy.zwerdling@gmail.com>
2018-03-08 12:35:18 +0200

    I update the script and render the string to parasha2.json

--- scripts/parallelscript.py
+++ scripts/parallelscript.py
@@ -1,6 +1,8 @@
 file = open("../app/NachRiddles/app/database/parasha.json", "r")
+output = open("../app/NachRiddles/app/database/parasha2.json", "w")
 
 read = file.read()
 read2 = read.rstrip()
 read3 = read.replace('\n',' ').replace('111','{ "parasha":"').replace('222','" },')
 print (read3,"check")
+output.write(read3)
