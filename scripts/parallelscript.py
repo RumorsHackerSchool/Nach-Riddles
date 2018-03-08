4e4509aa322444bdf8760de6146b9f51d9c0461a
guy.zwerdling <guy.zwerdling@gmail.com>
2018-03-08 12:21:48 +0200

    update the script and update the parasha.json with opening of 111 and closing of 222

--- scripts/parallelscript.py
+++ scripts/parallelscript.py
@@ -2,4 +2,5 @@ file = open("../app/NachRiddles/app/database/parasha.json", "r")
 
 read = file.read()
 read2 = read.rstrip()
-print (read2,"check")
+read3 = read.replace('\n',' ').replace('111','{ "parasha":"').replace('222','" },')
+print (read3,"check")
