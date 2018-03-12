b4ffefafdb4abd28849bbe53743d8b9535350644
guy.zwerdling <guy.zwerdling@gmail.com>
2018-03-12 13:17:59 +0200

    change the parasha script for find the error on the string in json file

--- scripts/parallelscript.py
+++ scripts/parallelscript.py
@@ -1,3 +1,4 @@
+'''
 file = open("../app/NachRiddles/app/database/parasha.json", "r")
 output = open("../app/NachRiddles/app/database/parasha2.json", "w")
 
@@ -6,3 +7,7 @@ read2 = read.rstrip()
 read3 = read.replace('\n',' ').replace('111','{ "parasha":"').replace('222','" },')
 print (read3,"check")
 output.write(read3)
+'''
+output = open("../app/NachRiddles/app/database/parasha2.json", "r")
+reader = output.read()
+print(reader[74244:74266])
